bb3841f0f80851f821ce8837a63a36c1a1219303
Nathan <56021106+H00La@users.noreply.github.com>
2022-03-07 12:16:56 +0100

    initial commit

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <div class="card-header">
+      Tasks
+    </div>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+      <b>{{ task.title }}</b> on the {{ task.date }} @ {{ task.time }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "TaskCard",
+  props: {
+    task: Object
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  border: 1px solid red;
+  width: 80%;
+  margin: auto;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/TasksList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="card" style="width: 24rem';">
+    <div class="card-header">
+      Tasks
+    </div>
+    <task-card v-for="task in tasks" :key="task.id" :task="task"></task-card>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TaskCard from "@/components/TaskCard.vue";
+
+export default {
+  name: "TasksList",
+  components: {
+    TaskCard
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Task 1",
+          description: "Description for Task 1",
+          location: "Valletta",
+          date: "1st of Jan",
+          time: "12:00"
+        },
+        {
+          id: 2,
+          title: "Task 2",
+          description: "Description for Task 2",
+          location: "Valletta",
+          date: "1st of Jan",
+          time: "12:00"
+        },
+        {
+          id: 3,
+          title: "Task 3",
+          description: "Description for Task 3",
+          location: "Valletta",
+          date: "1st of Jan",
+          time: "12:00"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  border: 1px solid green;
+  width: 90%;
+  margin: auto;
+}
+</style>
